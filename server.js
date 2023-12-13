85c9aacb9fc75fc574ed2e19ed9bb33ea1cd0e3b
Helen Medeiros <116672671+byhelenmedeiros@users.noreply.github.com>
2023-12-13 08:25:44 +0000

    trocar router

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const express = require('express');
-const router = express.Router();
 
 
 const router = require('./routes/index');
