5b9dbcaabddc45b5d94667c8304bc5577a22ef4e
Helen Medeiros <116672671+byhelenmedeiros@users.noreply.github.com>
2024-01-18 11:18:50 +0000

    funcionando

--- frontend/src/components/TaskList.js
+++ frontend/src/components/TaskList.js
@@ -1,46 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { ListGroup, Badge, Container } from 'react-bootstrap';
-import TaskForm from './TaskForm';
-import axios from 'axios';
 
-const TaskList = () => {
-  const [tasks, setTasks] = useState([]);
-  const [showAlert, setShowAlert] = useState(false);
-
-  useEffect(() => {
-    // Função para obter tarefas do backend
-    const fetchTasks = async () => {
-      try {
-        const response = await axios.get('http://localhost:5000/api/tasks');
-        setTasks(response.data);
-        setShowAlert(response.data.length === 0);
-      } catch (error) {
-        console.error('Erro ao obter tarefas:', error);
-      }
-    };
-
-    // Chamar a função para buscar tarefas ao montar o componente
-    fetchTasks();
-  }, []);
-
-  // Função para adicionar tarefa ao backend
-  const handleAddTask = async (newTask) => {
-    try {
-      const response = await axios.post('http://localhost:5000/api/tasks/create', newTask);
-      setTasks([...tasks, response.data]);
-    } catch (error) {
-      console.error('Erro ao adicionar tarefa:', error);
-    }
-  };
-
+const TaskList = ({ tasks }) => {
   return (
     <Container>
       <h2>Lista de Tarefas</h2>
-      {tasks.length === 0 && <p>Não há tarefas listadas.</p>}
-      {tasks.length > 0 && (
+      {tasks.length === 0 ? (
+        <p>Não há tarefas listadas.</p>
+      ) : (
         <ListGroup>
-          {tasks.map(task => (
-            <ListGroup.Item key={task.id}>
+          {tasks.map((task, index) => (
+            <ListGroup.Item key={index}>
               <strong>{task.title}</strong>
               <p>{task.description}</p>
               <Badge bg="success">Concluída</Badge>
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../components/Header';
 import TaskForm from '../components/TaskForm';
 import TaskList from '../components/TaskList';
 import { Container, Row, Col } from 'react-bootstrap';
 
 const HomePage = () => {
+  const [tasks, setTasks] = useState([]);
+
+  const handleAddTask = (newTask) => {
+    setTasks([...tasks, newTask]);
+  };
+
   return (
     <div>
       <Header />
@@ -12,10 +18,10 @@ const HomePage = () => {
         <Row className="mt-4">
           <Col md={6}>
             <h2>Adicionar Tarefa</h2>
-            <TaskForm />
+            <TaskForm onAddTask={handleAddTask} />
           </Col>
           <Col md={6}>
-            <TaskList />
+            <TaskList tasks={tasks} />
           </Col>
         </Row>
       </Container>
